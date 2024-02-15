6690b026e3f3be864883d288084105d0ebefa648
Hendrik Jung <hendrik.jung@physik.fu-berlin.de>
2024-02-15 13:25:50 +0100

    black formatting

--- src/writ/transform/svdframe.py
+++ src/writ/transform/svdframe.py
@@ -18,8 +18,8 @@ ArrayNx3 = Annotated[npt.NDArray[DType], Literal["N", 3]]
 ArrayMxNx3 = Annotated[npt.NDArray[DType], Literal["M", "N", 3]]
 
 
-#TODO: write tests!
-#TODO: option to use a subset of the atoms to do SVD frame fit 
+# TODO: write tests!
+# TODO: option to use a subset of the atoms to do SVD frame fit
 class RotateToSVDFrame(Iterable[A]):
     """Provides an iterable that rotates a source configuration into its SVD-frame
     and returns rotated coords and forces.
@@ -54,23 +54,30 @@ class RotateToSVDFrame(Iterable[A]):
         self.forces_idx = forces_idx
         self.aux_args = kwargs
 
-    def _rotate(self, coords_batch: ArrayMxNx3, forces_batch: ArrayMxNx3) -> Tuple[ArrayMxNx3, ArrayMxNx3]:
+    def _rotate(
+        self, coords_batch: ArrayMxNx3, forces_batch: ArrayMxNx3
+    ) -> Tuple[ArrayMxNx3, ArrayMxNx3]:
         rotated_coords = []
         rotated_forces = []
         for coords, forces in zip(coords_batch, forces_batch):
             c_rot, f_rot = self._rotate_single_frame(coords=coords, forces=forces)
             rotated_coords += [c_rot]
             rotated_forces += [f_rot]
-        return np.concatenate(rotated_coords, axis=0), np.concatenate(rotated_forces, axis=0)
+        return (
+            np.concatenate(rotated_coords, axis=0),
+            np.concatenate(rotated_forces, axis=0),
+        )
 
-    def _rotate_single_frame(self, coords: ArrayNx3, forces: ArrayNx3) -> Tuple[ArrayNx3, ArrayNx3]:
+    def _rotate_single_frame(
+        self, coords: ArrayNx3, forces: ArrayNx3
+    ) -> Tuple[ArrayNx3, ArrayNx3]:
         # need at least 3 atoms to compute SVD frame
         if coords.shape[0] > 2:
             U, S, Vh = np.linalg.svd(coords, full_matrices=False, compute_uv=True)
             V = Vh.T
             # NOTE: np.matmul is slower than mat1 @ mat2...
-            rotated_coords = coords @ V #np.matmul(coords, Vh.T)
-            rotated_forces = forces @ V #np.matmul(forces, Vh.T)
+            rotated_coords = coords @ V  # np.matmul(coords, Vh.T)
+            rotated_forces = forces @ V  # np.matmul(forces, Vh.T)
         else:
             rotated_coords = coords
             rotated_forces = forces
@@ -81,9 +88,10 @@ class RotateToSVDFrame(Iterable[A]):
         for pull in self.source:
             coords_batch = pull[self.coords_idx]
             forces_batch = pull[self.forces_idx]
-            coords_batch_rot, forces_batch_rot = self._rotate(coords_batch=coords_batch,
-                                                              forces_batch=forces_batch,
-                                                              )
+            coords_batch_rot, forces_batch_rot = self._rotate(
+                coords_batch=coords_batch,
+                forces_batch=forces_batch,
+            )
             # repack the pull
             # NOTE: we use [:] to reassign to the underlying array directly because
             #       tuples cant be mutated
