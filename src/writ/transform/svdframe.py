1e3eb412c55efb98890a2789730c3596ffb5d189
Hendrik Jung <hendrik.jung@physik.fu-berlin.de>
2024-02-28 17:26:02 +0100

    RotatetoSVDframe: put shape checks into its own method, add option to use a subset of the atoms for SVD fit

--- src/writ/transform/svdframe.py
+++ src/writ/transform/svdframe.py
@@ -2,6 +2,7 @@
 
 The RotateToSVDFrame class provides this functionality.
 """
+
 from typing import (
     Iterable,
     Iterator,
@@ -10,6 +11,7 @@ from typing import (
     Sequence,
     Literal,
     Tuple,
+    TypeGuard,
 )
 
 import numpy as np
@@ -24,8 +26,7 @@ ArrayMxNx3 = Annotated[npt.NDArray[DType], Literal["M", "N", 3]]
 
 
 # TODO: write tests!
-# TODO: option to use a subset of the atoms to do SVD frame fit
-class RotateToSVDFrame(Iterable[A]):
+class RotateToSVDFrame(Iterable[Sequence[A]]):
     """Provides an iterable that rotates a source configuration into its SVD-frame.
 
     This class returns rotated coords and forces by modifying the respective entries
@@ -38,7 +39,7 @@ class RotateToSVDFrame(Iterable[A]):
         source: Iterable[Sequence[A]],
         coords_idx: int = 0,
         forces_idx: int = 1,
-        check_types: bool = True,
+        atom_stride_for_svd: int = 1,
     ) -> None:
         """Initialize.
 
@@ -52,16 +53,17 @@ class RotateToSVDFrame(Iterable[A]):
         forces_idx: int
             Index of the forces array in the tuples we iterate over,
             by default 1.
-        check_types: bool
-            Whether we should check if the types and shapes of coordinates
-            and forces match what we expect,
-            by default True.
+        atom_stride_for_svd: int
+            Stride to use when choosing atoms for SVD fit,
+            by default 1.
+            Note that if the stride would result in using less than 3 atoms,
+            we will fallback to a stride of 1.
 
         """
         self.source = source
         self.coords_idx = coords_idx
         self.forces_idx = forces_idx
-        self.check_types = check_types
+        self.atom_stride_for_svd = atom_stride_for_svd
 
     def _rotate(
         self, coords_batch: ArrayMxNx3, forces_batch: ArrayMxNx3
@@ -81,8 +83,15 @@ class RotateToSVDFrame(Iterable[A]):
         self, coords: ArrayNx3, forces: ArrayNx3
     ) -> Tuple[ArrayNx3, ArrayNx3]:
         # need at least 3 atoms to compute SVD frame
-        if coords.shape[0] > 2:
-            U, S, Vh = np.linalg.svd(coords, full_matrices=False, compute_uv=True)
+        if coords.shape[0] // self.atom_stride_for_svd < 3:
+            coords_subset = coords
+        else:
+            coords_subset = coords[:: self.atom_stride_for_svd]
+        # (again) make sure we have enough atoms to calculate svd frame
+        if coords_subset.shape[0] > 2:
+            U, S, Vh = np.linalg.svd(
+                coords_subset, full_matrices=False, compute_uv=True
+            )
             V = Vh.T
             # NOTE: np.matmul is slower than mat1 @ mat2...
             rotated_coords = coords @ V  # np.matmul(coords, Vh.T)
@@ -92,28 +101,35 @@ class RotateToSVDFrame(Iterable[A]):
             rotated_forces = forces
         return rotated_coords, rotated_forces
 
+    def _check_coords_forces_shapes_match(
+        self, coords_batch: np.ndarray, forces_batch: np.ndarray
+    ) -> TypeGuard[ArrayMxNx3]:
+        coords_shape = coords_batch.shape
+        forces_shape = forces_batch.shape
+        assert len(coords_shape) == 3
+        assert coords_shape[-1] == 3
+        for dim_c, dim_f in zip(coords_shape, forces_shape):
+            assert dim_c == dim_f
+        return True
+
     def __iter__(self) -> Iterator[Sequence[A]]:
-        """Iterate over input, returning rotated coords and forces."""
+        "Iterate over input, returning rotated coords and forces."
         for pull in self.source:
             coords_batch = pull[self.coords_idx]
             forces_batch = pull[self.forces_idx]
-            if self.check_types:
-                assert isinstance(coords_batch, np.ndarray)
-                assert isinstance(forces_batch, np.ndarray)
-                coords_shape = coords_batch.shape
-                forces_shape = forces_batch.shape
-                assert len(coords_shape) == 3
-                assert coords_shape[-1] == 3
-                for dim_c, dim_f in zip(coords_shape, forces_shape):
-                    assert dim_c == dim_f
-
-            coords_batch_rot, forces_batch_rot = self._rotate(
-                coords_batch=coords_batch,
-                forces_batch=forces_batch,
-            )
-            # repack the pull
-            # NOTE: we use [:] to reassign to the underlying array directly because
-            #       tuples cant be mutated
-            pull[self.coords_idx][:] = coords_batch_rot
-            pull[self.forces_idx][:] = forces_batch_rot
-            yield pull
+            if (
+                isinstance(coords_batch, np.ndarray)
+                and isinstance(forces_batch, np.ndarray)
+                and self._check_coords_forces_shapes_match(coords_batch, forces_batch)
+            ):
+                coords_batch_rot, forces_batch_rot = self._rotate(
+                    coords_batch=coords_batch,
+                    forces_batch=forces_batch,
+                )
+                # repack the pull
+                # NOTE: coords_batch and forces_batch are references to the entries in pull,
+                #       we use [:] to reassign to the underlying array directly because
+                #       tuples cant be mutated
+                coords_batch[:] = coords_batch_rot
+                forces_batch[:] = forces_batch_rot
+                yield pull
