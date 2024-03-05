ec4df25dd90adcd324695608d0b9b4fa4b483e52
Aleksander Durumeric <alekepd@users.noreply.github.com>
2024-03-05 15:24:57 +0100

    Added pytest framework, github test CI, and tests for schemah5 class. (#6)
    * Added github test CI and tests for schemah5 class.
    
    * Added xxhash to install requirements.

---
+++ test/h5_test.py
@@ -0,0 +1,153 @@
+"""Test h5 readers.
+
+Note that some SchemaH5 does not have a stable order of output between python sessions; 
+as a result, we do not rely on chunk ordering being stable.
+"""
+
+from typing import Final
+from pathlib import Path
+import pytest
+from tools import arrhash, ihash, ghash
+
+from writ.read import SchemaH5
+from writ.util import TupleStrider
+
+AAQAA_H5_PATH: Final = (
+    Path(__file__).parent / Path("data") / Path("aaqaa_stride_10000_9ramp.h5py")
+)
+AAQAA_COORD_KEY: Final = "coords"
+AAQAA_FORCE_KEY: Final = "Fs"
+
+
+@pytest.fixture
+def aaqaa_coord_h5() -> SchemaH5:
+    """Produce SchemaH5 of aaqaa.
+
+    Returns coord arrays in tuples, no ids.
+    """
+    return SchemaH5(
+        target=str(AAQAA_H5_PATH),
+        schema=[AAQAA_COORD_KEY],
+        include_id=False,
+        singleton=False,
+    )
+
+
+@pytest.fixture
+def aaqaa_coord_force_h5() -> SchemaH5:
+    """Produce SchemaH5 of aaqaa.
+
+    Returns coord and force arrays in tuples, no ids.
+    """
+    return SchemaH5(
+        target=str(AAQAA_H5_PATH),
+        schema=[AAQAA_COORD_KEY, AAQAA_FORCE_KEY],
+        include_id=False,
+        singleton=False,
+    )
+
+
+@pytest.fixture
+def aaqaa_coord_h5_singleton() -> SchemaH5:
+    """Produce SchemaH5 of aaqaa.
+
+    Returns coord arrays (no tuples), no ids.
+    """
+    return SchemaH5(
+        target=str(AAQAA_H5_PATH),
+        schema=["coords"],
+        include_id=False,
+        singleton=True,
+    )
+
+
+@pytest.fixture
+def aaqaa_coord_h5_with_ids() -> SchemaH5:
+    """Produce SchemaH5 of aaqaa.
+
+    Returns coord arrays with ids in tuples.
+    """
+    return SchemaH5(
+        target=str(AAQAA_H5_PATH),
+        schema=[AAQAA_COORD_KEY],
+        include_id=True,
+        singleton=False,
+    )
+
+
+@pytest.fixture
+def aaqaa_coord_h5_stride_2() -> SchemaH5:
+    """Produce SchemaH5 of aaqaa.
+
+    Returns coord arrays (strided by 2) in tuples, no ids.
+    """
+    return SchemaH5(
+        target=str(AAQAA_H5_PATH),
+        schema=[AAQAA_COORD_KEY],
+        include_id=False,
+        transform=TupleStrider(stride=2),
+        singleton=False,
+    )
+
+
+@pytest.mark.h5py
+def test_schemah5_read(aaqaa_coord_h5: SchemaH5) -> None:
+    """Test accuracy of basic single-array SchemaH5 reading.
+
+    Content hash is checked against saved value.
+    """
+    CORRECT_HASH: Final = 5588434593584076510
+    # the read entries are size one tuples, so we extract the array via [0]
+    content_hash = ihash((x[0] for x in aaqaa_coord_h5), hasher=arrhash, order=False)
+    assert CORRECT_HASH == content_hash
+
+
+@pytest.mark.h5py
+def test_schemah5_strided_read(
+    aaqaa_coord_h5: SchemaH5, aaqaa_coord_h5_stride_2: SchemaH5
+) -> None:
+    """Test accuracy of strided SchemaH5 reading.
+
+    Data is read using built in striding and compared to manual striding.
+    """
+    # the read entries are size one tuples, so we extract the array via [0]
+    strided_content_hash = ihash(
+        (x[0] for x in aaqaa_coord_h5_stride_2), hasher=arrhash, order=False
+    )
+    manual_strided_content_hash = ihash(
+        (x[0][::2] for x in aaqaa_coord_h5), hasher=arrhash, order=False
+    )
+    assert strided_content_hash == manual_strided_content_hash
+
+
+@pytest.mark.h5py
+def test_schemah5_ids(aaqaa_coord_h5_with_ids: SchemaH5) -> None:
+    """Test accuracy id_included SchemaH5 reading.
+
+    Hash is compared to saved value.
+    """
+    CORRECT_HASH: Final = 11890363317478772579
+    content_hash = ihash(
+        (x for x in aaqaa_coord_h5_with_ids), hasher=ghash, order=False
+    )
+    assert CORRECT_HASH == content_hash
+
+
+@pytest.mark.h5py
+def test_schemah5_singleton(
+    aaqaa_coord_h5_singleton: SchemaH5, aaqaa_coord_h5: SchemaH5
+) -> None:
+    """Test accuracy of singleton read.
+
+    Hash is compared to saved value and output of reading without id.
+    """
+    CORRECT_HASH: Final = 5588434593584076510
+    # the read entries are size one tuples, so we extract the array via [0]
+    content_hash = ihash(
+        (x for x in aaqaa_coord_h5_singleton), hasher=arrhash, order=False  # type: ignore
+    )
+    assert CORRECT_HASH == content_hash
+    nos_content_hash = ihash(
+        (x[0] for x in aaqaa_coord_h5), hasher=arrhash, order=False
+    )
+    assert nos_content_hash == content_hash
---
+++ test/tools.py
@@ -0,0 +1,145 @@
+"""Basic tools used by tests."""
+from typing import Final, Iterable, Callable, TypeVar, Union, Tuple, Optional
+import numpy as np
+import xxhash
+
+INT_MAX_BYTES: Final = 64
+INT_BYTE_ORDER: Final = "big"
+
+
+def arrhash(array: np.ndarray, /, include_shape: bool = True) -> int:
+    """Hash a numpy array.
+
+    Deterministic between processes.
+
+    Arguments:
+    ---------
+    array:
+        Array to hash. Positional argument only.
+    include_shape:
+        If true, we also add each integer in the shape tuple to the hash.
+
+    Returns:
+    -------
+    int representing the hash.
+
+    Notes:
+    -----
+    This function probably some size limits, but it's not clear. Should be
+    stable across different machines. Probably cannot tell the difference in
+    arrays that have different classes but identical data, which may be good or
+    bad.
+
+    """
+    h = xxhash.xxh64()
+    h.update(array.data.tobytes())
+    if include_shape:
+        for entry in array.shape:
+            h.update(entry.to_bytes(INT_MAX_BYTES, INT_BYTE_ORDER))
+    return h.intdigest()
+
+
+def strhash(s: str, /) -> int:
+    """Hash string.
+
+    Arguments:
+    ---------
+    s:
+        string to hash. Positional argument only.
+
+    Returns:
+    -------
+    integer representing hash.
+
+    """
+    h = xxhash.xxh64()
+    h.update(s)
+    return h.intdigest()
+
+
+def ghash(data: Union[np.ndarray, str, Tuple[str]], /) -> int:
+    """Hash data entry that may be of different types.
+
+    Wraps other hash methods.  Deterministic between processes.
+
+    Arguments:
+    ---------
+    data:
+        str or np.ndarray or tuple of str/np.ndaray. If np.ndarray, arrhash is called;
+        if str, strhash is called; if a tuple, ihash with ghash is called.
+
+    Returns:
+    -------
+    Int representing the hash.
+
+    Notes:
+    -----
+    This is a convenience function when it is unknown whether the argument of
+    an unspecified type. Currently only supports strings, tuples of strings/ndarrays,
+    and np.ndarrays.
+
+    """
+    if isinstance(data, np.ndarray):
+        return arrhash(data)
+    elif isinstance(data, str):
+        return strhash(data)
+    elif isinstance(data, tuple):
+        return ihash(data, hasher=ghash)
+    else:
+        raise NotImplementedError(f"Unknown how to hash {data}.")
+
+
+T = TypeVar("T")
+
+
+def ihash(
+    data: Iterable[T],
+    /,
+    hasher: Callable[[T], int],
+    order: bool = True,
+    seed: Optional[str] = "ahakwvota2",
+    **kwargs,
+) -> int:
+    """Hash an iterable of objects given a underlying hash function.
+
+    Deterministic between processes.
+
+    Warning:
+    -------
+    Hash does not change if the type of iterable changes.
+
+    Arguments:
+    ---------
+    data:
+        Iterable arrays to hash. Position only argument.
+    hasher:
+        Callable which is used to hash each entry in data. These produced hashes
+        are then hashed. If data is already hashed, set to lambda x: x. Should
+        return an int for each item.
+    order:
+        If true, the hash will change if the order of arrays changes. If false,
+        it should not. False behavior is imposed via sorted acting on the hashes.
+    seed:
+        Value to feed into the hasher prior to hashing iterates. Allows the produced
+        hash of a single entry iterable to not be identical to a similar hash function
+        applied applied to the iterable content. If None, no value is used.
+    **kwargs:
+        Passed to hasher.
+
+    Returns:
+    -------
+    Int representing the hash.
+
+    """
+    hashes = [hasher(x, **kwargs) for x in data]
+    h = xxhash.xxh64()
+    if seed is not None:
+        h.update(seed)
+    if order:
+        target = hashes
+    else:
+        target = sorted(hashes)
+    for entry in target:
+        h.update(entry.to_bytes(INT_MAX_BYTES, INT_BYTE_ORDER))
+
+    return h.intdigest()
